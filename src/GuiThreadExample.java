36ee3db0009db6aef8d7dc53f1725d15002d89c5
mbaltic <mbaltic@htl-steyr.ac.at>
2018-12-11 09:41:39 +0100

    zufällige Buttons werden grün angezeigt

--- src/GuiThreadExample.java
+++ src/GuiThreadExample.java
@@ -8,36 +8,33 @@ import java.util.Random;
 
 public class GuiThreadExample extends JFrame {
 
-    private static int x=4;
-    private static int y=4;
+    private static int x = 4;
+    private static int y = 4;
 
 
     private JButton topButton = new JButton("Start");
     private JButton bottomButton = new JButton("Click me");
-   // private ButtonThread [][] buttonField = new ButtonThread[x][y];
-    private JPanel JPanel = new JPanel((new GridLayout(x,y)));
+    // private ButtonThread [][] buttonField = new ButtonThread[x][y];
+    private JPanel panel = new JPanel((new GridLayout(x, y)));
     private boolean fRun;//permanentes durchlaufen des Programmes
 
     public GuiThreadExample() {
         super("Threads und GUI");
         this.setSize(1200, 900);
 
-        this.add(JPanel);
+        this.add(panel);
         this.setVisible(true);
-
-
 
 
         List<JButton> buttonList = new ArrayList<>();
         int i = 0;
 
         for (i = 1; i <= 16; i++) {
-            JButton button = new JButton("Button " + i);
-            JPanel.add(button);
+            JButton button = new JButton("");
+            panel.add(button);
             button.setEnabled(true);
-            //button.setBackground(Color.GREEN);
+            button.setBackground(Color.GRAY);
         }
-
 
 
         Random random = new Random();
@@ -45,11 +42,14 @@ public class GuiThreadExample extends JFrame {
 
         this.setLayout(new BorderLayout());
 
-        this.add(JPanel, BorderLayout.CENTER);
+        this.add(panel, BorderLayout.CENTER);
         this.add(topButton, BorderLayout.NORTH);
         this.add(bottomButton, BorderLayout.SOUTH);
 
+        topButton.setBackground(Color.BLUE);
+
         bottomButton.setEnabled(false);
+
 
         topButton.addActionListener(new ActionListener() {
             @Override
@@ -63,16 +63,12 @@ public class GuiThreadExample extends JFrame {
                             e.printStackTrace();
                         }
 
-                        bottomButton.setBackground(Color.BLACK);
-
+                        bottomButton.setBackground(Color.BLUE);
                     }
 
                 }.start();
-
-
             }
         });
-
 
 
         bottomButton.addActionListener(new ActionListener() {
@@ -84,28 +80,47 @@ public class GuiThreadExample extends JFrame {
             }
         });
 
-       /* private void run(ButtonThread[][] spielfeld){
-            fRun = true;
-            int time = 0;
-            while (fRun){
-                boolean noGreen = true;
-                int checkGreen = 0;
-                for(int i=0;i<y;i++){
-                    for(int j=0;j<x;j++){
-                        if (spielfeld[i][j].getBackground() == Color.GREEN){
-                            checkGreen++;
-
-                        }
-                    }
-
-                }
-            }
-
-        }
-        */
-
-
         this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         this.setVisible(true);
+
+        run(panel);
+    }
+
+    private void run (JPanel p) {
+        fRun = true;
+        int time = 0;
+        while (fRun) {
+            boolean noGreen = true;
+            int checkGreen = 0;
+
+            for (int i = 0; i < p.getComponentCount(); ++i) {
+                    if (p.getComponent(i).getBackground() == Color.GREEN) {
+                        checkGreen++;
+
+                    }
+            }
+            if (checkGreen > 0) {
+                noGreen = false;
+            }
+            if (noGreen) {
+                try {
+                    Thread.sleep(time);
+                    Random rand = new Random();
+                    int buttonAmount = rand.nextInt(3) + 1;
+
+                    for (int k = 0; k < buttonAmount; k++) {
+                        Random r = new Random();
+
+                        panel.getComponent(r.nextInt(15)).setBackground(Color.GREEN);
+                        //p[i][j].startThread();
+
+                    }
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
     }
 }
+
